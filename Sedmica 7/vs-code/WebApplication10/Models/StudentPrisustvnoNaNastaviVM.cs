15841344e5047dbd7a1cb53b817d9f6a32c9ac32
Adil <adil@fit.ba>
2020-11-19 11:57:58 +0100

    19.11.2020. kraj prvoj časa

---
+++ Sedmica 7/vs-code/WebApplication10/Models/StudentPrisustvnoNaNastaviVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication10.Models
+{
+    public class StudentPrisustvnoNaNastaviVM
+    {
+        public class Zapis
+        {
+            public string Predmet { get; set; }
+            public DateTime Datum { get; set; }
+        }
+        public string ImeStudenta { get; set; }
+        public IEnumerable<Zapis> Zapisi { get; set; }
+    }
+}
